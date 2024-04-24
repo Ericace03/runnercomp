bb96243a936ed38c48bb430ecb50c8c1f18f7ca2
boroserika <ericacebmf@gmail.com>
2024-04-24 20:32:41 +0200

    springboot init

---
+++ src/main/java/gde/runnercomp/RunnercompApplication.java
@@ -0,0 +1,13 @@
+package gde.runnercomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnercompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnercompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gde/runnercomp/RunnercompApplicationTests.java
@@ -0,0 +1,13 @@
+package gde.runnercomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnercompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
